33bfe8a1742eed86573ce4697bc2c5ca05568d5b
Ryan Vurva <ryanvurva@gmail.com>
2017-05-06 01:43:36 -0400

    home page set up, basically

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,9 @@ class Home extends Component {
     return <div className='Home'>
       <div className='Hello'>
         <img src={ryan} alt='ryan vurva' width='200px' height='200px' />
+        <p className='line-1 anim-typewriter'>Hi, I'm Ryan Vurva.</p>
+        <p className='line-2 anim-typewriter-2'>I'm a Junior Developer in the Tampa Bay Area.</p>
+        {/* <a href='#' className='fancy-btn'>Would you like to know more?</a> */}
       </div>
     </div>
   }
