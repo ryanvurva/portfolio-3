0b1ce0fc4f1f1e7318c4a777f6f62e227aeea3bf
Ryan Vurva <ryanvurva@gmail.com>
2017-05-06 13:08:08 -0400

    home page polished with some cool animations

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,11 @@ class Home extends Component {
   render () {
     return <div className='Home'>
       <div className='Hello'>
-        <img src={ryan} alt='ryan vurva' width='200px' height='200px' />
+        <img className='me' src={ryan} alt='ryan vurva' width='200px' height='200px' />
         <p className='line-1 anim-typewriter'>Hi, I'm Ryan Vurva.</p>
-        <p className='line-2 anim-typewriter-2'>I'm a Junior Developer in the Tampa Bay Area.</p>
-        {/* <a href='#' className='fancy-btn'>Would you like to know more?</a> */}
+        <p className='line-2 anim-typewriter-2'>I'm a Junior Developer</p>
+        <p className='line-3 anim-typewriter-3'>in the Tampa Bay Area.</p>
+        <a href='#' className='fancy-btn'>Would you like to know more?</a>
       </div>
     </div>
   }
