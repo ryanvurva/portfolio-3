c17544b6e74e0c41d2610686a1763bf43064b3cb
Ryan Vurva <ryanvurva@gmail.com>
2017-06-23 17:44:43 -0400

    about me changes

--- src/components/About.js
+++ src/components/About.js
@@ -3,11 +3,22 @@ import React, { Component } from 'react'
 class About extends Component {
   render () {
     return <div className='About'>
-      <div className='who'>
-        Who Am I?
-      </div>
       <div className='aboutMe'>
-        Ryan is a poop head who likes to take poops and stuff.  how about you?
+        <div className='aboutMeHeader fade-in-1'>
+          Would you like to know more?
+        </div>
+        <div className='description fade-in-2'>
+          I'm a web developer in the Tampa Bay, FL area.  I have a number of things that I am passionate about,
+          but web design and programming have always been high on my list of interests.  I grew an affinity for the tech industry
+          in high school learning about graphic arts and production printing/screen printing, but it was the tech design part that always held my attention.
+          After high school I began working in web design and development as a side option to a career in the service industry as a Bartender.  After 10+
+          years of this, I decided to enroll in the Iron Yard, St. Pete.  I learned to refine my basic programming skills, and learned Javascript with a
+          strong focus on ReactJS.  I also have a background using Wordpress.org for various websites.  My other passions include being a father/husband, music,
+          movies and the film industry, and much more.  My goal is to take every project given to me, and to the best of my ability, move that project forward...
+        </div>
+      </div>
+      <div className='skills'>
+        ...
       </div>
     </div>
   }
