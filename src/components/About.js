1c2780300cc24f7f42ece295f89ccbe6ec5e7ec8
Ryan Vurva <ryanvurva@gmail.com>
2017-06-22 22:39:01 -0400

    about sample

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 class About extends Component {
   render () {
     return <div className='About'>
-      ...
+      <div className='who'>
+        Who Am I?
+      </div>
     </div>
   }
 }
