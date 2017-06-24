15c2bba2547c42765377cfdfdc944528996991b6
Ryan Vurva <ryanvurva@gmail.com>
2017-06-23 22:52:08 -0400

    skills added to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react'
 
+import codeLogos from '../images/code-logos.png'
+import Sass from '../images/sass.png'
+import ReactLogo from '../images/react.png'
+import MobX from '../images/mobx.png'
+import Wordpress from '../images/wordpress-logo-long.png'
+import Slack from '../images/slack-logo.png'
+import GitHub from '../images/github-logo.png'
+
 class About extends Component {
   render () {
     return <div className='About'>
@@ -18,7 +26,29 @@ class About extends Component {
         </div>
       </div>
       <div className='skills'>
-        ...
+        <div className='Skills'>
+          <div className='base-languages'>
+            <img src={codeLogos} alt='html 5, css 3, javascript es2016' title='html 5, css 3, javascript es2016' />
+          </div>
+          <div className='sass'>
+            <img src={Sass} alt='sass' title='sass' />
+          </div>
+          <div className='mobx'>
+            <img src={MobX} alt='mob x' title='mobx' />
+          </div>
+          <div className='reactlogo'>
+            <img src={ReactLogo} alt='react' title='react' />
+          </div>
+          <div className='wordpress'>
+            <img src={Wordpress} alt='wordpress' title='wordpress' />
+          </div>
+          <div className='slack'>
+            <img src={Slack} alt='slack' title='slack' />
+          </div>
+          <div className='github'>
+            <img src={GitHub} alt='github' title='github' />
+          </div>
+        </div>
       </div>
     </div>
   }
