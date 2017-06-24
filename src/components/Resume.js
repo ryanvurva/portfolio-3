0382420218f1334ce6fd5a047b383423d3428827
Ryan Vurva <ryanvurva@gmail.com>
2017-06-23 22:15:15 -0400

    resume on site now

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Resume extends Component {
   render () {
     return <div className='Resume'>
-      ...
+      <iframe src='https://docs.google.com/document/d/1Llnj8LuXJO2XQJJA8dwHxrZfoZz0tOKMOB2BM9j2Rgc/pub?embedded=true' />
     </div>
   }
 }
